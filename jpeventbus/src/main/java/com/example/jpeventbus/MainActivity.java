47fdf464dc5d15529a7249fdaf66e8f205f570b3
androidjp <1049578026@qq.com>
2016-05-31 15:00:00 +0800

    补充修改的提交

--- jpeventbus/src/main/java/com/example/jpeventbus/MainActivity.java
+++ jpeventbus/src/main/java/com/example/jpeventbus/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.jpeventbus;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import com.example.jpeventbus.model.HttpUtil;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HttpUtil.method2();
     }
 }
